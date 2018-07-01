0b9512a4f3f0b366129dbde47937c017ed0cdb0d
최병민(01045842518) <byung803@gmail.com>
2018-07-01 21:22:37 +0900

    Textarea simple alert complete

--- src/components/TextareaTest.js
+++ src/components/TextareaTest.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const TextareaTest = (props) => {
-    return (
-        <div>TextareaTest</div>
-    );
+class TextareaTest extends Component {
+    state = { note: '' }
+
+    onNoteChange = (e) => {
+        this.setState({ note: e.target.value });
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state.note);
+        alert(this.state.note);
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <textarea value={this.state.note} onChange={this.onNoteChange} placeholder="placeholder" />
+                <input type="submit" />
+            </form>
+        );
+    }
 }
 
 export default TextareaTest;
