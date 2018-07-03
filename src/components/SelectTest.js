bb7b463f0bfd065b4a9d6570e71d89b763f38d7d
최병민(01045842518) <byung803@gmail.com>
2018-07-03 23:22:40 +0900

    select form ~ing / setState callback func fixing need

--- src/components/SelectTest.js
+++ src/components/SelectTest.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SelectTest = (props) => {
-    return (
-        <div>SelectTest</div>
-    );
+class SelectTest extends Component {
+    state = { value: '' }
+
+    handleSelect = (e) => {
+        // console.log(e.target);
+        // console.log(e.target.value);
+        // this.setState(() => ({ value: e.target.value }));
+        this.setState({ value: e.target.value });
+        // this.setState(() => {
+        //     return { value: e.target.value };
+        // });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    <select value={this.state.value} onChange={this.handleSelect}>
+                        <option value="react">React</option>
+                        <option value="vue">Vue</option>
+                        <option value="angular">Angular</option>
+                        <option value="reactNative">ReactNative</option>
+                        <option value="jquery">Jquery</option>
+                    </select>
+                </label>
+                <input type="submit" value="제출" />
+            </form>
+        );
+    }
 }
 
 export default SelectTest;
